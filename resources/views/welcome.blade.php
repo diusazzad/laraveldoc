085e40182fe375654355258929347a1927784f3c
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2023-06-09 19:24:07 +0600

    making roadmap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel documentation</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-          @vite('resources/css/app.css')
-    </head>
+    <title>Laravel documentation</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite('resources/css/app.css')
+</head>
 
 </html>
+
+<body>
+
+    <div class="" id="mainframe">
+        <div class="border" id="content">
+            <div class="" id="routing">
+                <h1>
+                     Routing
+                </h1>
+                <ul class="" id="">
+
+                    <li class="" id="">Refirect Routes</li>
+                    <li class="" id=""></li>
+                    <li class="" id=""></li>
+
+                </ul>
+
+                <ul class=""></ul>
+                <ul class="">Basic Routing</ul>
+                <ul class="">Basic Routing</ul>
+                <ul class="">Basic Routing</ul>
+                <ul class="">Basic Routing</ul>
+            </div>
+        </div>
+    </div>
+</body>
